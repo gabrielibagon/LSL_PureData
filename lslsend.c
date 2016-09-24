fcce71a8fb8faef42b8503768ff854b81db37ada
gabrielibagon <gabriel.ibagon@gmail.com>
2016-09-24 09:18:52 -0700

    first commit of lslsend

---
+++ lslsend.c
@@ -0,0 +1,158 @@
+/* lslsend.c
+*
+*
+*
+*/
+
+#include "m_pd.h"      //pd header file
+#include "lsl_c.h"     //LSL header file
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_EVENT_LENGTH 4096
+#define DEFAULT_STREAM_NAME "pd_send"
+#define DEFAULT_STREAM_TYPE "EEG"
+#define DEFAULT_DATA_TYPE "string"
+#define DEFAULT_NCHAN 1
+#define MAX_ARG_LENGTH 50
+#define MAX_DATA_TYPE_LENGTH 32
+
+
+//TODO: any need to expose the lsl timestamp of event?
+static t_class *lslsend_class;
+
+typedef struct _lslsend{
+	t_object m_obj;
+    
+    char eventcode[MAX_EVENT_LENGTH];            /* current event code */
+	lsl_streaminfo lsl_info;     //the streaminfo returned by the resolve call 
+	char  lsl_stream_name[MAX_ARG_LENGTH]; /* Stream Name */
+	char lsl_stream_type[MAX_ARG_LENGTH];
+    int lsl_nchan;              /* number of channels in the stream (speacified when creating object) */
+	lsl_channel_format_t lsl_channel_format;
+    char data_type[MAX_ARG_LENGTH]; /* ui specified data type */
+
+	lsl_outlet lsl_outlet;		/* a stream outlet to push events to */
+ 	int lsl_errcode;			/* error code (lsl_lost_error or timeouts) */
+    double lsl_timestamp;		/* time stamp of the current sample (in sender time) */
+
+} t_lslsend;
+
+
+
+void* lslsend_new(t_symbol* s, long argc, t_atom* argv);
+void  lslsend_free(t_lslsend* x);
+void  lslsend_assist(t_lslsend* x, void* b, long m, long a, char* s);
+void  lslsend_bang(t_lslsend *x);
+void  lslsend_anything(t_lslsend *x, t_symbol *s, long argc, t_atom *argv);
+
+void* lslsend_new(t_symbol* s, long argc, t_atom* argv){
+    
+    // DEBUG print out input args
+    // printargs(s, argc, argv);
+    // DEBUG print out input args
+    // t_lslreceive *x = (t_lslreceive *)pd_new(lslreceive_class);
+
+	t_lslsend *x = (t_lslsend *)pd_new(lslsend_class);
+	
+	if (x != NULL) {
+                
+        // get event stream name if specified, else use default
+        if (argc>=1 && argv[0].a_type==A_SYMBOL) {
+            strncpy(x->lsl_stream_name, atom_getsymbol(&argv[0])->s_name, MAX_ARG_LENGTH);
+        } else {
+            strncpy(x->lsl_stream_name, DEFAULT_STREAM_NAME, MAX_ARG_LENGTH);
+            post(" Using default stream name '%s'",x->lsl_stream_name);
+        }
+        /* Stream type */
+		if (argc>=2 && argv[1].a_type==A_SYMBOL){
+		    strncpy(x->lsl_stream_type,atom_getsymbol(&argv[1])->s_name, MAX_ARG_LENGTH);
+		} else {
+		    strncpy(x->lsl_stream_type, DEFAULT_STREAM_TYPE, MAX_ARG_LENGTH);
+		    post(" Using default stream type (%s)",x->lsl_stream_type);
+		}
+		/* Number of Channels */
+		if (argc>=3 && argv[2].a_type==A_FLOAT) {
+		    x->lsl_nchan = atom_getint(&argv[2]);
+		    if (x->lsl_nchan < 0) {
+		        x->lsl_nchan = 1;
+		        post("Warning: Must specify at least one channel. Defaulting to one channel.");
+		    }
+		} else {
+		    post(" Using default number of channels (%d).",DEFAULT_NCHAN);
+		    x->lsl_nchan = DEFAULT_NCHAN;
+		}
+		/* Channel format */
+		if (argc>=4 && argv[3].a_type==A_SYMBOL) {
+		    strncpy(x->data_type, atom_getsymbol(&argv[3])->s_name, MAX_DATA_TYPE_LENGTH);
+		} else {
+		    strncpy(x->data_type, DEFAULT_DATA_TYPE, MAX_DATA_TYPE_LENGTH);
+		    post(" Using default data type (%s)",x->data_type);
+		}
+
+		// handle data-type specifics
+		if (!strcmp(x->data_type, "string") || !strcmp(x->data_type, "string32")) {
+		    x->lsl_channel_format = cft_string;
+		} else if (!strcmp(x->data_type, "float") || !strcmp(x->data_type, "float32")) {
+		    x->lsl_channel_format = cft_float32;
+		} else {
+		    post("ERROR: Unsupported data type (%s)",x->data_type);
+		    return NULL;
+		}
+
+        
+        x->eventcode[0]=0; //probably unnecessary--ensure event code is empty to start
+				
+		post("Creating an event Markers stream named '%s'.",x->lsl_stream_name);
+		x->lsl_info = lsl_create_streaminfo(x->lsl_stream_name,x->lsl_stream_type,x->lsl_nchan,0,x->lsl_channel_format,"uniqueid12345");
+	    x->lsl_outlet = lsl_create_outlet(x->lsl_info, 0, 300);
+	   
+        if (x->lsl_outlet) {
+            post("Stream created.\n");
+        } else {
+            post("Problem creating stream. Events won't be sent.");
+        }
+	}
+	
+	return x;
+}
+
+void lslsend_setup(void) {  
+  lslsend_class = class_new(gensym("lslsend"),  
+							    (t_newmethod)lslsend_new,  
+							    (t_method)lslsend_free,
+							    sizeof(t_lslsend),  
+							    CLASS_DEFAULT,
+							    A_GIMME,
+							   	0);   
+	class_addbang(lslsend_class, (t_method)lslsend_bang);
+	class_addmethod(lslsend_class, (t_method)lslsend_anything, "anything", A_GIMME, 0);
+}
+
+
+
+void lslsend_free(t_lslsend* x)
+{
+	/* Do any deallocation needed here. */
+    lsl_destroy_outlet(x->lsl_outlet);
+}
+
+// send the event code on a bang
+void  lslsend_bang(t_lslsend *x) {
+    
+    // post("eventcode length= %d",strlen(x->eventcode));
+    
+    if (strlen(x->eventcode) && x->lsl_outlet) {
+        //post("bang, sending code %s:",x->eventcode);
+        char *ptr = x->eventcode;
+        lsl_push_sample_str(x->lsl_outlet, &ptr);
+    }
+}
+
+// store then send first symbol in input out as event string
+void  lslsend_anything(t_lslsend *x, t_symbol *s, long argc, t_atom *argv) {
+    strncpy(x->eventcode, s->s_name, MAX_EVENT_LENGTH);
+    //post("set event code to: %s",x->eventcode);
+    lslsend_bang(x);
+}
